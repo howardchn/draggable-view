62b4332a1e65cad09cab26f917dd73b780e24f5e
howardchn <howardch@outlook.com>
2015-02-06 17:58:24 +0800

    Init

--- PanningTest/ViewController.swift
+++ PanningTest/ViewController.swift
@@ -9,17 +9,84 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var tile : UIView = UIView()
+    var labelView = UITextView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tile.frame = CGRect(x: 0, y: 0, width: 256, height: 256)
+        tile.backgroundColor = UIColor.redColor()
+        view.addSubview(tile)
+        
+        var panGesture = UIPanGestureRecognizer(target: self, action: Selector("panHandler:"))
+        view.addGestureRecognizer(panGesture)
+        
+        var pinchGesture = UIPinchGestureRecognizer(target: self, action: Selector("pinchHandler:"))
+        view.addGestureRecognizer(pinchGesture)
+        
+        labelView.frame = CGRect(x: 0, y: 100, width: view.frame.width, height: 44)
+        labelView.backgroundColor = UIColor.clearColor()
+        view.addSubview(labelView)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func panHandler (p: UIPanGestureRecognizer!) {
+        var translation = p.translationInView(view)
+        if (p.state == UIGestureRecognizerState.Began) {
+            
+        }
+        else if (p.state == UIGestureRecognizerState.Changed) {
+            var offsetX = translation.x
+            var offsetY = translation.y
+            
+            var newLeft = tile.frame.minX + offsetX
+            var newTop = tile.frame.minY + offsetY
+            
+            self.tile.frame = CGRect(x: newLeft, y: newTop, width: self.tile.frame.width, height: self.tile.frame.height)
+            labelView.text = "x: \(newLeft); y: \(newTop)"
+            p.setTranslation(CGPoint.zeroPoint, inView: view)
+        }
+        else if (p.state == UIGestureRecognizerState.Ended) {
+//            var inertia = p.velocityInView(view)
+//            var offsetX = inertia.x * 0.2
+//            var offsetY = inertia.y * 0.2
+//            var newLeft = tile.frame.minX + offsetX
+//            var newTop = tile.frame.minY + offsetY
+//            
+//            UIView.animateWithDuration(0.3, delay: 0, options:UIViewAnimationOptions.CurveEaseOut, animations: {_ in
+//                self.tile.frame = CGRect(x: newLeft, y: newTop, width: self.tile.frame.width, height: self.tile.frame.height)
+//            }, completion: nil)
+        }
     }
-
-
+    
+    func pinchHandler (p: UIPinchGestureRecognizer!) {
+    
+    }
 }
+
+public class MyTile : UIView {
+    private let cellCount : Float = 10
+    
+    override public func drawRect(rect: CGRect) {
+        var cellSize = Float(rect.width) / cellCount
+        var context = UIGraphicsGetCurrentContext()
+        CGContextSetLineWidth(context, 1)
+        CGContextSetStrokeColorWithColor(context, UIColor.grayColor().CGColor)
+        
+        for var row : Float = 0; row <= cellCount; row++ {
+            var top = CGFloat(row * cellSize)
+            var pointsH = [CGPoint(x: 0, y: top), CGPoint(x: rect.width, y: top)]
+            CGContextAddLines(context, pointsH, UInt(pointsH.count))
+            
+            for var column : Float = 0; column <= cellCount; column++ {
+                var left = CGFloat(column * cellSize)
+                var pointsV = [CGPoint(x: left, y: 0), CGPoint(x: left, y: rect.height)];
+                CGContextAddLines(context, pointsV, UInt(pointsV.count))
+            }
+        }
+        
+        CGContextDrawPath(context, kCGPathStroke)
+    }
+}
+
 
